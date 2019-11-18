9f8c97f5cf356d6dc81feedccf0e7ecef6d9b04c
Nanoteck137 <patrik.millvik@gmail.com>
2019-11-18 08:32:04 +0100

    Doing some work on the interface

--- Mass/src/Program.cs
+++ Mass/src/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Reflection;
 using Mass.Compiler;
 
 namespace Mass
 {
     class Program
     {
+        static void PrintUsage(string executableName)
+        {
+            Console.WriteLine($"Usage: {executableName} [options] file");
+            Console.WriteLine($"Options:");
+            Console.WriteLine($"  --help     Display this infomation");
+            Console.WriteLine($"  --version  Display the version number");
+        }
+
+        static void PrintVersion(string executableName)
+        {
+            Console.WriteLine($"{executableName} - v0.1");
+        }
+
         static void Main(string[] args)
         {
+            string path = Assembly.GetEntryAssembly().Location;
+            string executableName = Path.GetFileNameWithoutExtension(path);
+            // PrintUsage(executableName);
+
+
+            Queue<string> arguments = new Queue<string>();
+            foreach (string arg in args)
+            {
+                arguments.Enqueue(arg);
+            }
+
+            string currentArg = arguments.Dequeue();
+            bool stop = false;
+            while (!stop && currentArg.StartsWith("--"))
+            {
+                if (currentArg == "--help")
+                {
+                    PrintUsage(executableName);
+                    stop = true;
+                    break;
+                }
+                else if (currentArg == "--version")
+                {
+                    PrintVersion(executableName);
+                    stop = true;
+                    break;
+                }
+
+                if (!arguments.TryDequeue(out currentArg))
+                {
+                    PrintUsage(executableName);
+                    break;
+                }
+            }
+
+            return;
             string filePath = "test.ma";
             filePath = Path.GetFullPath(filePath);
 
