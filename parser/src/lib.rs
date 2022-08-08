89232742ef850570882d3939278f893c7ca3889f
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-08-08 03:12:03 +0200

    Parsing and creating an AST from the parser output

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,11 +1,329 @@
 #[macro_use]
 extern crate pest_derive;
 
+#[macro_use]
+extern crate lazy_static;
+
+use pest::prec_climber::{Assoc, PrecClimber};
+use pest::iterators::{Pair, Pairs};
 use pest::Parser;
+
+use ast::{
+    Ident, Typespec, Decl, StmtBlock, FunctionParam, Stmt, Expr, BinaryOp, P,
+};
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct LangParser;
+
+lazy_static! {
+    static ref PREC_CLIMBER: PrecClimber<Rule> = {
+        use pest::prec_climber::Operator;
+        use Assoc::*;
+        use Rule::*;
+
+        PrecClimber::new(vec![
+            Operator::new(op_or, Left),
+            Operator::new(op_and, Left),
+            Operator::new(op_equal, Left) | Operator::new(op_notequal, Left),
+            Operator::new(op_lessthan, Left) |
+                Operator::new(op_lessthanequal, Left) |
+                Operator::new(op_greaterthan, Left) |
+                Operator::new(op_greaterthanequal, Left),
+            Operator::new(op_add, Left) | Operator::new(op_minus, Left),
+            Operator::new(op_multiply, Left) | Operator::new(op_divide, Left),
+        ])
+    };
+}
+
+#[derive(Debug)]
+pub struct ParserContext {
+    ident_table: Vec<String>,
+}
+
+impl ParserContext {
+    pub fn new() -> Self {
+        Self {
+            ident_table: Vec::new(),
+        }
+    }
+
+    pub fn add_ident(&mut self, ident: String) -> Ident {
+        let mut has_ident = None;
+        for index in 0..self.ident_table.len() {
+            if self.ident_table[index] == ident {
+                has_ident = Some(index);
+            }
+        }
+
+        return if let Some(index) = has_ident {
+            Ident::new(index)
+        } else {
+            let index = self.ident_table.len();
+            self.ident_table.push(ident);
+            Ident::new(index)
+        };
+    }
+
+    pub fn get_ident(&self, ident: Ident) -> &String {
+        &self.ident_table[ident.index()]
+    }
+}
+
+fn process_base_typespec(
+    parser_context: &mut ParserContext,
+    base_typespec: Pair<Rule>,
+) -> P<Typespec> {
+    let base = base_typespec.into_inner().next().unwrap();
+    match base.as_rule() {
+        Rule::ident => {
+            let ident = base.as_str();
+            let ident = parser_context.add_ident(ident.to_string());
+            Typespec::name(ident)
+        }
+
+        _ => unimplemented!("{:?}", base.as_rule()),
+    }
+}
+
+fn process_typespec(
+    parser_context: &mut ParserContext,
+    typespec: Pair<Rule>,
+) -> P<Typespec> {
+    let mut inner = typespec.into_inner();
+    let base_typespec = inner.next().unwrap();
+    let base_typespec = process_base_typespec(parser_context, base_typespec);
+
+    let mut result = base_typespec;
+    if let Some(after) = inner.next() {
+        match after.as_rule() {
+            Rule::pointer => {
+                for _ in 0..after.as_str().len() {
+                    result = Typespec::ptr(result);
+                }
+            }
+
+            _ => unimplemented!("{:?}", after.as_rule()),
+        }
+    }
+
+    result
+}
+
+fn create_expr_ast(
+    parser_context: &mut ParserContext,
+    expr: Pairs<Rule>,
+) -> P<Expr> {
+    PREC_CLIMBER.climb(
+        expr,
+        |pair: Pair<Rule>| match pair.as_rule() {
+            Rule::bin_op => process_expr(parser_context, pair),
+
+            Rule::integer => {
+                let s = pair.as_str();
+                Expr::integer(s.parse::<u64>().unwrap())
+            }
+
+            Rule::ident => {
+                let ident = pair.as_str();
+                let ident = parser_context.add_ident(ident.to_string());
+                Expr::ident(ident)
+            }
+
+            Rule::string => {
+                let s = pair.as_str();
+                let s = &s[1..s.len() - 1];
+                Expr::string(String::from(s))
+            }
+
+            Rule::expr => process_expr(parser_context, pair),
+
+            Rule::base => {
+                let mut inner = pair.into_inner();
+                let pair = inner.next().unwrap();
+
+                let expr = process_expr(parser_context, pair);
+
+                if let Some(next) = inner.next() {
+                    match next.as_rule() {
+                        Rule::func_call => {
+                            let mut args = next.into_inner();
+                            let args = args.next().unwrap().into_inner();
+
+                            let mut res = Vec::new();
+                            for arg in args {
+                                let expr = process_expr(parser_context, arg);
+                                res.push(expr);
+                            }
+
+                            Expr::call(expr, res)
+                        }
+
+                        Rule::array_index => {
+                            let index = process_expr(
+                                parser_context,
+                                next.into_inner().next().unwrap(),
+                            );
+
+                            Expr::index(expr, index)
+                        }
+
+                        _ => unimplemented!("{:?}", next.as_rule()),
+                    }
+                } else {
+                    expr
+                }
+            }
+
+            _ => unimplemented!("{:?}", pair.as_rule()),
+        },
+        |left: P<Expr>, op: Pair<Rule>, right: P<Expr>| {
+            let op = match op.as_rule() {
+                Rule::op_multiply => BinaryOp::Multiply,
+                Rule::op_divide => BinaryOp::Divide,
+
+                Rule::op_add => BinaryOp::Add,
+                Rule::op_minus => BinaryOp::Minus,
+
+                Rule::op_lessthan => BinaryOp::LessThan,
+                Rule::op_lessthanequal => BinaryOp::LessThanEqual,
+                Rule::op_greaterthan => BinaryOp::GreaterThan,
+                Rule::op_greaterthanequal => BinaryOp::GreaterThanEqual,
+
+                Rule::op_equal => BinaryOp::Equal,
+                Rule::op_notequal => BinaryOp::NotEqual,
+
+                Rule::op_and => BinaryOp::And,
+                Rule::op_or => BinaryOp::Or,
+
+                _ => unimplemented!("{:?}", op.as_rule()),
+            };
+
+            Expr::binary(op, left, right)
+        },
+    )
+}
+
+fn process_expr(
+    parser_context: &mut ParserContext,
+    expr: Pair<Rule>,
+) -> P<Expr> {
+    create_expr_ast(parser_context, expr.into_inner())
+}
+
+fn process_stmt(
+    parser_context: &mut ParserContext,
+    stmt: Pair<Rule>,
+) -> P<Stmt> {
+    let stmt = stmt.into_inner().next().unwrap();
+
+    match stmt.as_rule() {
+        Rule::stmt_var => {
+            let mut inner = stmt.into_inner();
+
+            let name = inner.next().unwrap().as_str();
+            let name = parser_context.add_ident(name.to_string());
+
+            let typespec = inner.next().unwrap();
+            let typespec = process_typespec(parser_context, typespec);
+
+            let expr = if let Some(expr) = inner.next() {
+                let expr = process_expr(parser_context, expr);
+                Some(expr)
+            } else {
+                None
+            };
+
+            Stmt::var(name, typespec, expr)
+        }
+
+        Rule::stmt_ret => {
+            let expr = stmt.into_inner().next().unwrap();
+            let expr = process_expr(parser_context, expr);
+
+            Stmt::ret(expr)
+        }
+
+        Rule::stmt_expr => {
+            let expr = stmt.into_inner().next().unwrap();
+            let expr = process_expr(parser_context, expr);
+
+            Stmt::expr(expr)
+        }
+
+        _ => unimplemented!("{:?}", stmt.as_rule()),
+    }
+}
+
+fn process_stmt_list(
+    parser_context: &mut ParserContext,
+    stmt_list: Pair<Rule>,
+) -> StmtBlock {
+    let mut result = StmtBlock::new();
+
+    for stmt in stmt_list.into_inner() {
+        let stmt = process_stmt(parser_context, stmt);
+        result.add_stmt(stmt);
+    }
+
+    result
+}
+
+fn process_block(
+    parser_context: &mut ParserContext,
+    block: Pair<Rule>,
+) -> StmtBlock {
+    process_stmt_list(parser_context, block.into_inner().next().unwrap())
+}
+
+fn process_decl_func(
+    parser_context: &mut ParserContext,
+    func: Pair<Rule>,
+) -> P<Decl> {
+    let mut inner = func.into_inner();
+
+    let name = inner.next().unwrap().as_str();
+
+    let func_param_list = inner.next().unwrap();
+    let mut params = Vec::new();
+    for func_param in func_param_list.into_inner() {
+        let mut inner = func_param.into_inner();
+
+        let name = inner.next().unwrap();
+        let typespec = inner.next().unwrap();
+        let typespec = process_typespec(parser_context, typespec);
+
+        let name = parser_context.add_ident(name.to_string());
+        params.push(FunctionParam::new(name, typespec));
+    }
+
+    let return_type = inner.next().unwrap();
+
+    let return_type = if let Some(typespec) = return_type.into_inner().next() {
+        let typespec = process_typespec(parser_context, typespec);
+        Some(typespec)
+    } else {
+        None
+    };
+
+    let body = inner.next().unwrap();
+    let body = process_block(parser_context, body);
+
+    let name = parser_context.add_ident(name.to_string());
+    Decl::function(name, params, return_type, body)
+}
+
+fn process_decl(
+    parser_context: &mut ParserContext,
+    decl: Pair<Rule>,
+) -> P<Decl> {
+    let decl = decl.into_inner().next().unwrap();
+
+    match decl.as_rule() {
+        Rule::func_decl => process_decl_func(parser_context, decl),
+        _ => unimplemented!("{:?}", decl.as_rule()),
+    }
+}
 
 pub fn parse(input: &str) {
     let file = LangParser::parse(Rule::file, input)
@@ -13,7 +331,12 @@ pub fn parse(input: &str) {
         .next()
         .unwrap();
 
-    for root in file.into_inner() {
-        println!("Root: {:#?}", root);
+    let mut parser_context = ParserContext::new();
+
+    // let decl_list = file.into_inner().next().unwrap();
+    for pair in file.into_inner() {
+        if pair.as_rule() == Rule::decl {
+            process_decl(&mut parser_context, pair);
+        }
     }
 }
--- test.ma
+++ test.ma
@@ -1,5 +1,11 @@
+
+func add(a: u32, b: u32) -> u32*** {
+    ret a + b;
+}
+
 // This is the main function
 func main(argc: s32, argv: s8**) -> u32 {
     var x: s32;
     scanf("%d", addr(x));
 }
+
